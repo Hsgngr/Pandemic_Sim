743196bdd202dffb364262f2d03b71e398d692d2
Ege <hsgngr@gmail.com>
2020-07-29 19:41:39 +0300

    Update SIR.cs

--- JustSimulation/Assets/Scripts/SIR.cs
+++ JustSimulation/Assets/Scripts/SIR.cs
@@ -11,15 +11,11 @@ public class SIR : MonoBehaviour
     private Text text;
     private PandemicArea pandemicArea;
 
-   public StringBuilder sb = new System.Text.StringBuilder();
- 
-
     // Start is called before the first frame update
     void Start()
     {
         pandemicArea = GetComponentInParent<PandemicArea>();
-        text = GetComponent<Text>();
-        sb.AppendLine("HealthyCount;InfectedCount;RecoveredCount;Time");
+        text = GetComponent<Text>();      
     }
 
     // Update is called once per frame
@@ -28,31 +24,5 @@ public class SIR : MonoBehaviour
         text.text = "Total Healthy Agents = " + pandemicArea.healthyCounter + "\n" +
                     "Total Infected Agents =" + pandemicArea.infectedCounter;
         
-    }
-
-    public string ToCSV()
-    {
-        decimal time = Decimal.Round((decimal)Time.time, 2);
-        sb.AppendLine(pandemicArea.healthyCounter.ToString() + ';' + pandemicArea.infectedCounter.ToString() + ";" + pandemicArea.recoveredCounter.ToString() +  ";" + time.ToString());
-        return sb.ToString();
-
-    }
-    public void SaveToFile()
-    {
-        // Use the CSV generation from before
-        var content = ToCSV();
-
-        // The target file path e.g.
-        var folder = Application.streamingAssetsPath;
-
-        if (!Directory.Exists(folder)) Directory.CreateDirectory(folder);
-
-
-        var filePath = Path.Combine(folder, "export.csv");
-
-        using (var writer = new StreamWriter(filePath, false))
-        {
-            writer.Write(content);
-        }
     }
 }
